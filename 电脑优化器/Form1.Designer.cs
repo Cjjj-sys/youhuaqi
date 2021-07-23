df6c013112be10607bdf01802426020299c5ebbe
jjj c <ws2005ncsd@outlook.com>
2021-07-23 17:04:34 +0800

    项目文件

---
+++ 电脑优化器/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace 电脑优化器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开启卓越性能";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "杀掉资源管理器";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "关掉程序本身";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(142, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(16, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "哈哈哈哈，你真笨。";
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(161, 49);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(8, 10);
+            this.tb.TabIndex = 5;
+            this.tb.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 135);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "电脑优化器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox tb;
+    }
+}
+
---
+++ 电脑优化器/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 电脑优化器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string GetCmdOutput(string arg,bool nowindow)
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.CreateNoWindow = nowindow;
+                //p.StartInfo.Arguments = arg;
+
+                p.Start();
+
+                p.StandardInput.WriteLine(arg);
+                p.StandardInput.WriteLine("exit");
+                //powercfg /list
+                return p.StandardOutput.ReadToEnd();
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+            
+        }
+
+        private void CmdRun(string arg, bool nowindow)
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.CreateNoWindow = nowindow;
+                //p.StartInfo.Arguments = arg;
+
+                p.Start();
+
+                p.StandardInput.WriteLine(arg);
+                p.StandardInput.WriteLine("exit");
+                //powercfg /list
+                //return p.StandardOutput.ReadToEnd();
+            }
+            catch (Exception)
+            {
+
+                //return "";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cfg =
+            GetCmdOutput("powercfg /list",true);
+
+            if (cfg.Contains("卓越性能"))
+            {
+                MessageBox.Show("已生成卓越性能,正在尝试开启...");
+                //cfg = cfg.Replace("Microsoft Windows [版本 10.0.19042.746]","");
+                //cfg = cfg.Replace("(c) 2020 Microsoft Corporation. 保留所有权利。","");
+                //cfg = cfg.Replace("C:\\Users\\ws200\\source\\repos\\电脑优化器\\电脑优化器\\bin\\Debug>powercfg /list","");
+                //cfg = cfg.Replace("现有电源使用方案 (* Active)","");
+                //cfg
+
+                //File.WriteAllText("xfg.txt",cfg);
+                tb.Text = cfg;
+                string[] tlist = tb.Lines;
+                int i = 1;
+                string id = "?";
+                foreach (var t in tlist)
+                {
+                    if (t.Contains("卓越性能")&&i==1&&!t.Contains("*"))
+                    {
+                        id = t.Replace("电源方案 GUID: ","");
+                        id = id.Replace("  (卓越性能)","");
+                        i++;
+                    }
+                }
+                if (id == "?")
+                {
+                    MessageBox.Show("你已经启用卓越性能");
+                }
+                else
+                {
+                    string result = GetCmdOutput("powercfg /S " + id, true);
+                    //string r = GetCmdOutput("powercfg /list", true);
+                    MessageBox.Show(result);
+                    string b = GetCmdOutput("powercfg /list", true);
+                    MessageBox.Show(b);
+                }
+                
+
+            }
+            else
+            {
+                string a = GetCmdOutput("powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61",true);
+                MessageBox.Show(a);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "杀掉资源管理器")
+            {
+                string a = GetCmdOutput("taskkill /f /im explorer.exe", true);
+                MessageBox.Show(a);
+                button2.Text = "打开资源管理器";
+            }
+            else
+            {
+                CmdRun("%SystemRoot%\\explorer.exe", true);
+                button2.Text = "杀掉资源管理器";
+                //MessageBox.Show(b);
+            }
+            
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
